8c19cbb5f41a48953582611cb42e40480e986841
Ryan Houck <ryan@levelskies.com>
2014-12-20 10:51:26 -0800

    began building splash page

--- statstrat1/urls.py
+++ statstrat1/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'statstrat1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'statstrat1.views.splash', name='splash'),
 )
---
+++ statstrat1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.contrib.auth.views import logout
+from django.contrib.auth.views import login
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.forms.util import ErrorList
+
+import datetime
+import json
+
+def splash(request):
+	
+	return render_to_response('splash.html', {}, context_instance=RequestContext(request))
