63ca96fdbe5d3dd4ed82c58e2af0abfaa3e42333
Ryan Houck <ryan@levelskies.com>
2014-12-20 18:35:37 -0800

    update splash page

--- model/p4.py
+++ model/p4.py
@@ -1,7 +1,7 @@
 from p3 import *
 
 if __name__ == "__main__":
-
+	"""
 	portfolio_returns = pd.io.parsers.read_csv('model_output/test_results.csv', index_col=0, parse_dates=True)
 
 	index_tix = ['^GSPC', '^IXIC']
@@ -12,3 +12,17 @@ if __name__ == "__main__":
 	portfolio_returns['index'] = index_weekly_returns
 
 	portfolio_returns_prod = portfolio_returns.cumprod().dropna(how='any')
+	"""
+
+	"""
+	tix = get_import_io_s_and_p_tickers()	
+	end_date = datetime.datetime(2011,7,1,0,0)
+	for i in range(5):
+		end_date = end_date - datetime.timedelta(days=91*i)
+		start_date = end_date - datetime.timedelta(days=91)
+
+
+		df = get_collection_as_pandas_df(tix, 'stocks_test', update=False)
+		w = Window(df, start_date=start_date, end_date=end_date, return_period_days=1)
+		w.pull_cointegrated_partners(date_strict=True)
+	"""
